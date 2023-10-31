4c1455b11f25c42da7516b42c78aca185e18dfb6
Shumon Khan <backlink.shumonkhan@gmail.com>
2023-10-31 12:23:43 +0600

    add page groud route for landing and dashboard

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1>This is a dashboard page</h1>
+    </div>
+  );
+};
+
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>This is the landing page</h1>
+    </div>
+  );
+};
+
+export default LandingPage;
