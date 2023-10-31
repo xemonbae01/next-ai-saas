30aa28abe18c12fef544b63cdf2127dfc44d77c3
Shumon Khan <backlink.shumonkhan@gmail.com>
2023-10-31 13:19:59 +0600

    complete auth sing in/up by clerk auth

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 const DashboardPage = () => {
   return (
     <div>
       <h1>This is a dashboard page</h1>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,19 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 const LandingPage = () => {
   return (
     <div>
       <h1>This is the landing page</h1>
+      <div>
+        <Link href={"/sign-in"}>
+          <Button>Log In</Button>
+        </Link>
+        <Link href={"/sign-up"}>
+          <Button>Log Up</Button>
+        </Link>
+      </div>
     </div>
   );
 };
