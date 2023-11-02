5105958d5a8b7fa084165154dd24256724c2f956
Shumon Khan <backlink.shumonkhan@gmail.com>
2023-11-03 00:14:31 +0600

    Complete Music AI Model

---
+++ app/(dashboard)/(routes)/music/page.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import { useState } from "react";
+import axios from "axios";
+import * as z from "zod";
+import { Music } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { formSchema } from "./constants";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Form, FormField, FormItem, FormControl } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { ChatCompletionMessageParam } from "openai/resources/chat/completions";
+import { Heading } from "@/components/Heading";
+import { Empty } from "@/components/Empty";
+import { Loader } from "@/components/Loader";
+
+const MusicPage = () => {
+  const router = useRouter();
+  const [music, setMusic] = useState<string>();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (value: z.infer<typeof formSchema>) => {
+    try {
+      setMusic(undefined);
+
+      const response = await axios.post("/api/music", value);
+
+      setMusic(response.data.audio);
+
+      //to clear the imput inside th form
+      form.reset();
+    } catch (error) {
+      //TO DO Open Pro Modal
+      console.log(error);
+    } finally {
+      router.refresh();
+    }
+  };
+
+  return (
+    <div>
+      <Heading
+        title="Music Generation"
+        description="Trun your prompt into a music"
+        icon={Music}
+        iconColor="text-emerald-500"
+        bgColor="bg-emerald-500/10"
+      />
+      <div className="px-4 md:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="rounded-lg w-full border p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
+            >
+              <FormField
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12  lg:col-span-10">
+                    <FormControl>
+                      <Input
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading}
+                        placeholder="Piano solo"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button
+                className="col-span-12 lg:col-span-2 w-full"
+                disabled={isLoading}
+              >
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className="mt-4 space-y-4">
+          {isLoading && (
+            <div className="p-8 rounded-lg w-full flex items-center justify-center">
+              <Loader />
+            </div>
+          )}
+          {!music && !isLoading && <Empty label="No music generated" />}
+          {music && (
+            <audio controls className="w-full mt-8">
+              <source src={music} />
+            </audio>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MusicPage;
---
+++ app/api/music/route.ts
@@ -0,0 +1,57 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import Replicate from "replicate";
+
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_KEY,
+});
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { prompt } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorize", { status: 401 });
+    }
+
+    if (!prompt) {
+      return new NextResponse("Music prompt is required", { status: 400 });
+    }
+
+    //***this is a lengthly process without webhook**
+    // const response = await replicate.run(
+    //   "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
+    //   {
+    //     input: {
+    //       alpha: 0.5,
+    //       prompt_a: prompt,
+    //       denoising: 0.75,
+    //       num_inference_steps: 50,
+    //     },
+    //   }
+    // );
+    //***this is a lengthly process without webhook**
+
+    //generating music using web hook
+    const response = await replicate.run(
+      "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
+      {
+        input: {
+          alpha: 0.5,
+          prompt_a: prompt,
+          denoising: 0.75,
+          num_inference_steps: 50,
+          // Add the webhook URL here
+          webhook: process.env.REPLICATE_WEBHOOK_URL,
+        },
+      }
+    );
+
+    return NextResponse.json(response);
+  } catch (error) {
+    console.log("[MUSIC_ERROR]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
